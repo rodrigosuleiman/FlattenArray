b95a4232592216fb0d6d651f60c8835988f3cf6c
rodrigosuleiman <45185096+rodrigosuleiman@users.noreply.github.com>
2019-01-06 18:17:24 -0200

    Add files via upload

---
+++ CitrusByte/FlattenArray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CitrusByte
+{
+    public static class FlattenArrayTest
+    {
+        /// <summary>
+        //Arrays of integers into a flat array of integers.e.g. [[1, 2,[3]],4] -> [1,2,3,4]. 
+        /// </summary>
+        public static int[] Flatten(ArrayList arr)
+        {
+            List<int> result = new List<int>();
+            try
+            {
+                Recursive(arr, ref result);
+            }
+            catch
+            {
+                result.Clear();
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Recursive method to flatten
+        /// </summary>
+        private static void Recursive(ArrayList arr, ref List<int> ret)
+        {
+            if (ret == null)
+            {
+                ret = new List<int>();
+            }
+
+            for (int i = 0; i < arr.Count; ++i)
+            {
+                if (arr[i] is ArrayList)
+                {
+                    Recursive(arr[i] as ArrayList, ref ret);
+                }
+                else if (arr[i] is int)
+                {
+                    ret.Add((int)arr[i]);
+                }
+                else
+                {
+                    throw new Exception("Cannot be flatten");
+                }
+            }
+        }
+    }
+}
---
+++ CitrusByteTest/InterviewExerciseTests.cs
@@ -0,0 +1,24 @@
+﻿using CitrusByte;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace CitrusByte.Tests
+{
+    [TestClass()]
+    public class FlattenTests
+    {
+       
+        [TestMethod()]
+        public void SampleTestSholdBePass()
+        {
+            // [[1,2,[3]],4] → [1,2,3,4]
+            ArrayList test = new ArrayList() { new ArrayList() { 1, 2, new ArrayList() { 3 } } , 4 };
+
+            int[] expectedResult = new int[] { 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expectedResult, FlattenArrayTest.Flatten(test));
+        }
+        
+
+      
+    }
+}
